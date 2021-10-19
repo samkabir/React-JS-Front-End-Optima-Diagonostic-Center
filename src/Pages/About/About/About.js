9051503a6ff4f6c240c5de39062bf5f10cea01a5
samkabir <samkabir26@gmail.com>
2021-10-19 18:50:34 +0600

    Completed most of the requirements

--- src/Pages/About/About/About.js
+++ src/Pages/About/About/About.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h2>About</h2>
+        <div className="container">
+            <h2 className="text-primary">The new way to healty life</h2>
+            <div className="we-are">
+                <h3 className="text-primary">Who We Are</h3>
+                <p>Optima Diagnostic Centre Ltd. is an advanced Centre for diagnostic and medical services. It is one of the prestigious diagnostic complexes of Bangladesh which started its activities in 1983. Optima Diagnostic Centre Ltd. is the largest diagnostic services provider organization in private sector of the country. It is been pioneer in introducing world latest medical equipments and advanced technology to provide round the clock medical investigations and consultancy services.</p>
+                <br />
+            </div>
+            <div className="goals">
+                <h3 className="text-primary">Our Goal</h3>
+                <p>To establish a referral Diagnostic and Medical Services Centre.</p>
+                <br />
+            </div>
+            <div className="objs">
+                <h3 className="text-primary">Our Objective</h3>
+                <ul>
+                    <li>To render the world standard diagnostic service to the people of the country at an affordable cost and in turn to limit the outflow of the patient abroad at the expense of heard earn foreign currency by providing quality diagnostic services.</li>
+                    <li>Out door basis treatment by renowned General Practitioners, Consultants and Professors in different medical fields.</li>
+                    <li>To promote Health Education & Medical Services.</li>
+                    <li>Day care Centre for follow-up cardiac renal and oncology patients.</li>
+                    <li>To build a full fledged specialized (Tertiary) Hospital.</li>
+                    <li>Set up Satellite collection Centre.</li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import './Appointment.css'
 
 const Appointment = () => {
     return (
-        <div>
-            <h2>Appointment</h2>
+        <div className="container">
+            <h2 className="text-primary">Appointment</h2>
+            <div className="appointment-container">
+                <form className="row gy-2 gx-3 align-items-center">
+                    <div className="col-auto appointment">
+                        <h6>Choose a Doctor</h6>
+                        <label className="visually-hidden" for="autoSizingSelect">Preference</label>
+                        <select className="form-select" id="autoSizingSelect">
+                        <option selected>Choose...</option>
+                        <option value="1">Dr Asaduzzaman Rasel MBBS, FCPS(ENT)</option>
+                        <option value="2">Prof. Dr. A K M Mustafa Hussain · MBBS, DTCD, MD (Chest)</option>
+                        <option value="3">Prof. Dr. Zeenat Meraj</option>
+                        <option value="4">Dr. Fahmida Zabeen</option>
+                        <option value="5">Dr. Mohammad Rofiqul Islam</option>
+                        <option value="6">Dr Md. Shah Alam</option>
+                        </select>
+                    </div>
+                    <div className="appointment">
+                        <h6>Name</h6>
+                        <div className="col-auto">
+                            <label className="visually-hidden" for="autoSizingInput">Name</label>
+                            <input type="text" className="form-control" id="autoSizingInput" placeholder="Name" />
+                        </div>
+                    </div>
+                    <div className="appointment">
+                        <h6>Address</h6>
+                        <div className="col-auto">
+                            <label className="visually-hidden" for="autoSizingInput">Name</label>
+                            <input type="text" className="form-control" id="autoSizingInput" placeholder="Address" />
+                        </div>
+                    </div>
+                    <div className="appointment">
+                        <h6>Phone Number</h6>
+                        <div className="col-auto">
+                            <label className="visually-hidden" for="autoSizingInput">Name</label>
+                            <input type="number" className="form-control" id="autoSizingInput" placeholder="Phone Number" />
+                        </div>
+                    </div>
+                    <div className="appointment">
+                        <h6>Preferred Date</h6>
+                        <div className="col-auto">
+                        <input type="date" id="appointment" name="appointment" />
+                        </div>
+                    </div>
+                    <div className="col-auto apt-btn">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
